ffd783240ffc454b0ac54c8344eda525dae1cc30
Daria <dariabatiuk5@gmail.com>
2023-05-10 18:39:23 -0300

    folder structure

---
+++ src/assets/index.js
@@ -0,0 +1,19 @@
+import calculator from "./images/Calculator.jpg";
+import cloudbudget from "./images/CloudBudget.jpg";
+import favicon from "./images/foto for Favicon.jpg";
+import ipad from "./images/ipad.jpg";
+import mobile from "./images/Mobile.jpg";
+import projectTracker from "./images/NB_Project_Tracker.jpg";
+import pulse from "./images/pulse.jpg";
+import wikitube from "./images/Wikitube.jpg";
+
+export{
+	calculator,
+	cloudbudget,
+	favicon,
+	ipad,
+	mobile,
+	projectTracker,
+	pulse,
+	wikitube,
+}
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+			fontFamily:{
+				bodyFont: ["Poppins", "sans-serif"],
+				titleFont: ["Montserrat", "sans-serif"],
+			},
+			colors: {
+				bodyColor: "#212428",
+				lightText: "#c4cfde",
+				designColor: "#ff014",
+			},
+			boxShadow:{
+				shadowOne: "10px 10px 19px #1c1e22, -10px -10px 19px #262a2e",
+			},
+		},
+  },
+  plugins: [],
+}
