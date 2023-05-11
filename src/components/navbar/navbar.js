e42c549cc0fb2877f858bbafecbf24ef4769d770
Daria <dariabatiuk5@gmail.com>
2023-05-11 12:26:16 -0300

    added banner + features components + fixed links

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { navLinksdata } from "../../constants";
+import {Link} from 'react-scroll';
 
 const Navbar = () => {
   return (
@@ -8,7 +9,14 @@ const Navbar = () => {
         <ul className="w-full h-20 mx-auto flex justify-end items-center gap-10 font-titleFont">
           {navLinksdata.map(({ _id, title, link }) => (
             <li className="text-base font-normal text-gray-400 tracking-wide cursor-pointer hover:text-designColor duration-300" key={_id}>
-              {title}
+              <Link
+							activeClass="active"
+							to={link}
+							spy={true}
+							smooth={true}
+							offset={-70}
+							duration={500}
+							>{title}</Link>
             </li>
           ))}
         </ul>
