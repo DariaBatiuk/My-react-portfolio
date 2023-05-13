5afaf4192f1acbb2960adc95794a028195f55553
Daria <dariabatiuk5@gmail.com>
2023-05-13 19:41:12 -0300

    added social links

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {useTypewriter, Cursor} from "react-simple-typewriter";
+import {FaFacebookF, FaLinkedin, FaReact, FaGithub} from "react-icons/fa";
 
 const Banner = () => {
 	const [text] = useTypewriter({
@@ -11,8 +12,8 @@ const Banner = () => {
   });
 
   return (
-    <section id="home" className="w-full pt-10 pb-20 flex border-b-1 border-black font-titleFont">
-      <div className="w-1/2">
+    <section id="home" className="w-full py-20 flex border-b-1 border-black font-titleFont">
+      <div className="w-1/2 flex flex-col gap-20">
 				<div className="flex flex-col gap-3">
 					<h4 className="text-lg font'normal">Welcome to my website</h4>
 					<h1 className="text-6xl font-bold text-white">
@@ -31,6 +32,22 @@ const Banner = () => {
 						<p className="text-base font-bodyFont leading-6 tracking-wide">
 						Motivated front-end developer with expertise in developing and maintaining user-friendly, responsive, and cross-browser compatible websites and web applications using HTML, CSS, JavaScript and React
 						</p>
+				</div>
+				<div>
+					<h2 className="text-base uppercase font-titleFont mb-4">
+						Find me in
+					</h2>
+					<div className="flex gap-4">
+						<span className="bannerIcon">
+							<FaFacebookF/>
+						</span>
+						<span className="bannerIcon">
+							<FaLinkedin/>
+						</span>
+						<span className="bannerIcon">
+							<FaGithub/>
+						</span>
+					</div>
 				</div>
 			</div>
 			<div className="w-1/2"></div>
