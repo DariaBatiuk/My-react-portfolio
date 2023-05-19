0b61ec398701f1de09c8af252e30123f736cf0da
Daria <dariabatiuk5@gmail.com>
2023-05-19 18:49:55 -0300

    added onclick for resume components

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Title from "../layouts/title";
- import Education from "./education";
+import Education from "./education";
 import Skills from "./skills";
 import Experience from "./experience";
 import ResumeCard from "./resumeCard";
 
-
-
 const Resume = () => {
+  const [educationData, setEducationData] = useState(true);
+  const [skillsData, setSkillsData] = useState(false);
+  const [experienceData, setExperienceData] = useState(false);
   return (
     <section id="resume" className="w-full py-20 border-b-[1px] border-b-black">
       <div className="flex justify-center items-center text-center">
@@ -15,12 +16,35 @@ const Resume = () => {
       </div>
       <div>
         <ul className="w-full grid grid-cols-3">
-          <li className="resumeLi">Education</li>
-          <li className="resumeLi">Professional Skills</li>
-          <li className="resumeLi">Experience</li>
+          <li onClick={() => 
+						setEducationData(true) &
+						setSkillsData(false) &
+						setExperienceData(false)
+						} className="resumeLi">
+            Education
+          </li>
+          <li
+            onClick={() => 
+							setEducationData(false) & 
+							setSkillsData(true) &
+							setExperienceData(false)
+						}
+            className="resumeLi"
+          >
+            Professional Skills
+          </li>
+          <li 
+					onClick={() => 
+						setEducationData(false) &
+						setSkillsData(false) &
+						setExperienceData(true)
+						} 
+					className="resumeLi">Experience</li>
         </ul>
       </div>
-
+      {educationData && <Education />}
+			{skillsData && <Skills />}
+			{experienceData && <Experience />}
       <Education />
     </section>
   );
