30541cbcb2916a7bf704933b63eb80972aefbcd0
Daria <dariabatiuk5@gmail.com>
2023-06-15 15:39:53 -0300

    added experience content

--- src/components/resume/experience.js
+++ src/components/resume/experience.js
@@ -7,30 +7,30 @@ const Experience = () => {
   return (
 		<motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 5 }}>
 			   <div className="py-12 font-titleFont ">
-        <p className="text-sm text-designColor tracking-[4px]">some years</p>
-        <h2 className="text-4xl font-bold">Education name smth</h2>
       </div>
-      <div className=" mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+      <div className="w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
         <ResumeCard
-          title="Certificate, Fullstack development"
-          subtitle="University of New Brunswick (September 2022 - April 2023)"
-          des="300+ hours of hands-on course material and completions. Mastered skills in full-stack web development (front end: JavaScript, React; back-end: Node.js, Express.js; database: SQL, MongoDB)."
+          title="Front End Developer, Freelance"
+          subtitle="Moncton, NB. January 2022 – Present."
+          des="Developing and maintaining user-friendly, responsive, and cross-browser compatible websites and web applications using HTML, CSS, and JavaScript. 
+					Collaborating with designers, developers, and other team members to ensure that website designs and functionality meet project requirements and user needs.
+					Testing and debugging website and application code to identify and fix errors and ensure optimal performance.
+					"
         />
         <ResumeCard
-          title="Master's degree, Finance and credit"
-          subtitle="Financial University of the Russian Federation (October 2008 - June 2014)"
-          des="Comprehensive six-year program covering financial management, credit analysis, investment strategies, and risk management. Developed expertise in financial modeling, data analysis, and strategic financial decision-making. Well-prepared to excel in roles involving financial analysis, investment management, and credit risk assessment."
+          title="QA Tester, BMM Testlabs"
+          subtitle="Moncton, NB. April 2022 – November 2022"
+          des="Ensured the quality of clients' mobile applications by performing functionality, usability, and regression testing.
+					Tested configuration and compatibility of software on multiple hardware platforms, utilizing strong analytical skills to identify and resolve compatibility issues.
+					Contributed to the development and follow-up of test plans and materials, demonstrating exceptional attention to detail and organization skills.
+					Tracked, analyzed, recorded, and verified all software functionality discrepancies and development corrections.
+					"
         />
-        <ResumeCard
-    title="Udemy courses"
-    subtitle={[
-        "WEB developer bootcamp",
-        "The Complete 2022 Software Testing Bootcamp",
-				"API Testing Foundations"
-    ].map((subtitle, index) => (
-        <div key={index}>{subtitle}</div>
-    ))}
-/>
+          <ResumeCard
+          title="Other work experience"
+          subtitle="Customer Success Specialist, June 2016 – March 2021  "
+          des="Tranio, Bryansk, Russia."
+        />
       </div>
 		</motion.div>
   
